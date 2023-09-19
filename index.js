0a35b4d5d47c9b3dc7b426c27948fb229e6e0e17
diego <diego.maustem@gmail.com>
2023-09-19 11:24:55 -0300

    :tada: Implementação node + mercado pago

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const MercadoPago = require("mercadopago")
+const app = express()
+
+MercadoPago.configure({
+    sandbox: true,
+    access_token: "JIHFSIHFWUEHFIUWHEIFW"
+})
+
+app.get("/", (req, res) => {
+    res.send("App Running")
+})
+
+app.get("/pagar", async (req, res) => {
+
+    let id = Date.now().toString()
+    let emailDoPagador = "diego.skeep@gmail.com"
+
+    var dados = {
+        items: [
+            item = {
+                id: id,
+                title: "Playstation 4 slim",
+                quantity: 2,
+                currency_id: "BRL",
+                unit_price: parseFloat(255)
+            }
+        ],
+        payer:{
+            email: emailDoPagador
+        },
+        external_reference: id
+    }
+
+    try{
+        let pagamento = await MercadoPago.preferences.create(dados)
+        console.log(pagamento)
+        return res.redirect(pagamento.body.init_point)
+    }catch(err){
+        return res.send(err.message)
+    }
+})
+
+app.listen(3000, (req, res) => {
+    console.log("Aplication Run!")
+})
